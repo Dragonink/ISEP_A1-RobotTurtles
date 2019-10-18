f1ecae74e33a27d608705272ff8615a683d63ecf
Dragonink <Dragonink@users.noreply.github.com>
2019-10-18 19:44:25 +0200

    Initialized Maven project

---
+++ src/main/java/robotturtles/g45/App.java
@@ -0,0 +1,17 @@
+package robotturtles.g45;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
